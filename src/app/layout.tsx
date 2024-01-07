01daf1b3717f577fe4570af671be4cd2026dbf79
zachbharris <zachbharris94@gmail.com>
2024-01-07 13:20:08 -0600

    add analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import { cn } from "@/lib/utils";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -29,6 +30,7 @@ export default function RootLayout({
         )}
       >
         <div className="max-w-2xl mx-auto">{children}</div>
+        <Analytics />
       </body>
     </html>
   );
